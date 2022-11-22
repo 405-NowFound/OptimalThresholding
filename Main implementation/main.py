2e27e08c13dd25e725a352c360951f6a5083211d
BiancaNegoescu <72906162+BiancaNegoescu@users.noreply.github.com>
2022-11-22 19:11:21 +0000

    read csv

--- Main implementation/main.py
+++ Main implementation/main.py
@@ -4,4 +4,10 @@ def main():
     pass
 
 if __name__ == "__main__":
-    main()
+
+    with open('/workspaces/OptimalThresholding/Main implementation/tests/global.CSV', 'r') as csv_file:
+        reader = csv.reader(csv_file, delimiter=',')
+        optimalTreshholdings = []
+        for row in reader:
+	        print(row[0])
+	
