606d6adfc3792fe1405e2e30c56bd015d7bf30e3
Adrian-Valentin Gheorghe <adryan_valentin@yahoo.com>
2022-11-26 19:46:30 +0200

    implement for all input files [global]

--- Main_implementation/main.py
+++ Main_implementation/main.py
@@ -2,6 +2,7 @@ import csv
 import operator
 from itertools import combinations, permutations
 import math
+import os
 
 class csvParser:
     """
@@ -49,8 +50,8 @@ class Binarization:
         self.parser = csvParser() # .csv file parser
         self.idealTh = 0 # the value of the ideal treshold
         self.valuesTh = [] # values of all tresholds
-        self.goodCombinations = [] # list of combinations of operators which are more than 99.99%
-
+        self.combFreq = dict() # list of combinations of operators which are more than 99.99%
+        self.allCombinations = []
 
     def generateCombinations(self):
         operatorsList = self.parser.plusList + self.parser.minusList + self.parser.multiplyList + self.parser.divideList
@@ -62,47 +63,66 @@ class Binarization:
 
         return comb1, comb2, comb3, comb4
 
-    def computeResult(self, order, result):
-        for i in range(1, len(self.valuesTh)):
-            result = self.parser.operationMap[order[i - 1]](result, float(self.valuesTh[i]))
-            if result <= 1 and result > 0:
-                index = math.floor(result * 255)
-                if float(self.parser.fMeasures[index]) > 99.99:
-                    self.goodCombinations.append(order)
+    def generateCombinationsList(self):
+        allCombinations = []
+        comb1, comb2, comb3, comb4 = self.generateCombinations()
 
-    def findSolutions(self, inputFile):
-        self.parser.readCSV(inputFile)
-        self.idealTh = self.parser.tresholdings[0]
-        self.valuesTh = self.parser.tresholdings[1:15]
-
-        result = 0
-        if self.goodCombinations == []:
-            comb1, comb2, comb3, comb4 = self.generateCombinations()
-
-            for e1 in comb1:
-                for e2 in comb2:
-                    for e3 in comb3:
-                        for e4 in comb4:
-                            order = e1 + e2 + e3 + e4
-                            result = float(self.valuesTh[0])
-                            self.computeResult(order, result)
-            
-        else:
-            for order in self.goodCombinations:
-                result = float(self.valuesTh[0])
-                self.computeResult(order, result)
-
-        print(len(set(self.goodCombinations)))
-        # self.printCombinations()
+        for e1 in comb1:
+            for e2 in comb2:
+                for e3 in comb3:
+                    for e4 in comb4:
+                        order = e1 + e2 + e3 + e4
+                        allCombinations.append(order)
+        return allCombinations
 
     def printCombinations(self):
         for combination in self.goodCombinations:
             print(combination)
 
+    def computeResult(self, order):
+        result = float(self.valuesTh[0])
+        for i in range(1, len(self.valuesTh)):
+            result = self.parser.operationMap[order[i - 1]](result, float(self.valuesTh[i]))
+
+        if result >= 0 and result <= 1 :
+            index = math.floor(result * 255)
+            if float(self.parser.fMeasures[index]) > 80:
+                return True
+            else:
+                return False 
+
+    def findSolutionForOneFile(self, inputFile):
+        self.parser.readCSV(inputFile)
+        self.idealTh = self.parser.tresholdings[0]
+        self.valuesTh = self.parser.tresholdings[1:15]
+
+        for order in self.allCombinations:
+            if self.computeResult(order) is True:
+                if order in self.combFreq.keys():
+                    self.combFreq[order] += 1
+                else:
+                    self.combFreq[order] = 1
+
 def main():
-    inputFile = 'tests/global.CSV'
+
     binarization = Binarization()
-    binarization.findSolutions(inputFile)
+    inputPath = os.getcwd() + '/tests/global/test'
+    dir_list = os.listdir(inputPath)
+
+    binarization.allCombinations = binarization.generateCombinationsList()
+
+    index = 20
+    for inFile in dir_list:
+        print(inFile)
+        print(index)
+        inputFilePath = inputPath + '/' + str(inFile)
+        binarization.findSolutionForOneFile(inputFilePath)
+        index -= 1
+
+        if index == 0:
+            break
+
+    print(max(binarization.combFreq.values()))
   
 if __name__ == "__main__":
     main()
@@ -114,3 +134,32 @@ if __name__ == "__main__":
     - how to take as input all files from a folder
     https://stackoverflow.com/questions/18262293/how-to-open-every-file-in-a-folder
     """
+
+
+    # def computeResult(self, order, result):
+    #     goodCombForCurrentStep = []
+    #     for i in range(1, len(self.valuesTh)):
+    #         result = self.parser.operationMap[order[i - 1]](result, float(self.valuesTh[i]))
+    #      if result <= 1 and result > 0:
+    #             index = math.floor(result * 255)
+    #             if float(self.parser.fMeasures[index]) > 80:
+    #                 goodCombForCurrentStep = order
+
+    #     print(goodCombForCurrentStep)
+    #     return goodCombForCurrentStep
+
+    # def findSolutions(self, inputFile):
+    #     self.parser.readCSV(inputFile)
+    #     self.idealTh = self.parser.tresholdings[0]
+    #     self.valuesTh = self.parser.tresholdings[1:15]
+    #     result = 0      
+                        # result = float(self.valuesTh[0])
+                        # self.goodCombinations = self.computeResult(order, result)
+        # else:
+        #     for order in self.goodCombinations:
+        #         result = float(self.valuesTh[0])
+        #         good = self.computeResult(order, result)
+        #         # print(self.goodCombinations)
+
+        # print(len(set(allCombinations)))
+        # self.printCombinations()
